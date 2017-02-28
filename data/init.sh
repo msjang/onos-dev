09e92e0e5e3b2d32d774b0de7c9c84de73806ff3
msjang <msjang@kisti.re.kr>
2017-02-28 11:27:30 +0900

    add data/init.sh

---
+++ data/init.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/opennetworkinglab/onos
+
+mkdir Downloads; cd Downloads
+wget http://archive.apache.org/dist/karaf/3.0.5/apache-karaf-3.0.5.tar.gz
+wget http://archive.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
